6fca37b44687e8f380c2ab98b589c1e7f10f0d9f
Diego Caponera <diego.caponera@gmail.com>
2023-08-29 12:51:53 +0200

    Implement poc.

---
+++ bin/build.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import path from 'path';
+import { parse, render } from '../src/index.js';
+
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+
+const { argv } = yargs(hideBin(process.argv))
+  .command('$0', 'generates the html')
+  .option('readmePath', {
+    describe: 'README path',
+    default: path.join(process.cwd(), 'README.md'),
+  })
+  .option('distPath', {
+    describe: 'dist path',
+    default: path.join(process.cwd()),
+  })
+  .option('templatePath', {
+    describe: 'custom templates path',
+  })
+  .coerce('templatePath', (value) =>
+    path.isAbsolute(value) ? value : path.join(process.cwd(), value)
+  );
+
+const data = await parse({ ...argv });
+await render({ ...data, ...argv });
--- src/lib.js
+++ src/lib.js
@@ -1,66 +1,91 @@
-import path from "path";
-import matter from "gray-matter";
-import fs from "fs-extra";
-import { globby } from "globby";
-import { marked } from "marked";
-import ejs from "ejs";
-import prettify from "html-prettify";
+import path from 'path';
+import fs from 'fs-extra';
+import { globby } from 'globby';
+import matter from 'gray-matter';
+import { marked } from 'marked';
+import { gfmHeadingId } from 'marked-gfm-heading-id';
+import ejs from 'ejs';
+import prettify from 'html-prettify';
+import * as cheerio from 'cheerio';
 
-const templateFiles = await globby("*.ejs", {
-    cwd: new URL(`./templates`, import.meta.url),
-});
+marked.use(gfmHeadingId());
 
-const templates = (
+async function parseTemplates(templatesPath, options) {
+  if (!templatesPath) {
+    return {};
+  }
+
+  const templateFiles = await globby('*.ejs', { cwd: templatesPath });
+
+  return (
     await Promise.all(
-        templateFiles.map(async (fileName) => ({
-            id: path.basename(fileName, ".ejs"),
-            template: ejs.compile(
-                await fs.readFile(
-                    new URL(`./templates/${fileName}`, import.meta.url),
-                    "utf8"
-                ),
-                {
-                    openDelimiter: "{",
-                    closeDelimiter: "}",
-                }
-            ),
-        }))
+      templateFiles.map(async (fileName) => ({
+        id: path.basename(fileName, '.ejs'),
+        template: ejs.compile(
+          await fs.readFile(path.join(templatesPath, fileName), 'utf8'),
+          options
+        ),
+      }))
     )
-).reduce((memo, { id, template }) => ({ ...memo, [id]: template }), {});
+  ).reduce((memo, { id, template }) => ({ ...memo, [id]: template }), {});
+}
 
 function parseMarkdown(content) {
-    const parsed = marked(content);
-    const split = parsed.split(/(<!--[\s\S]*?-->)/g);
-    const output = [
-        {
-            id: "hero",
-            content: split[0],
-        },
-    ];
-    for (let i = 1; i < split.length; i += 2) {
-        output.push({
-            id: split[i].match(/(<!--([\s\S]*?)-->)/)[2].trim(),
-            content: split[i + 1],
-        });
-    }
-    return output;
+  const split = marked(content).split('<hr>');
+
+  return [
+    {
+      id: 'top',
+      title: 'Home',
+      content: split[0],
+    },
+    ...split.slice(1).map((html) => {
+      const $ = cheerio.load(html);
+      const title = $('h2');
+      return {
+        content: html,
+        id: title.attr('id'),
+        title: title.text(),
+      };
+    }),
+  ];
 }
 
-export async function parse(readmePath = "./README.md") {
-    const readme = await fs.readFile(readmePath, "utf8");
-    const { content, data: meta } = matter(readme);
-    return {
-        sections: parseMarkdown(content),
-        meta,
-    };
+export async function parse({ readmePath = './README.md' }) {
+  const readme = await fs.readFile(readmePath, 'utf8');
+  const { content, data: meta } = matter(readme);
+  return {
+    sections: parseMarkdown(content),
+    meta,
+  };
 }
 
-export async function render({ sections, meta, distPath = "./dist" }) {
-    const outputPath = path.join(distPath, "index.html");
-    const output = templates.main({
-        title: meta.title,
-        body: sections.map(templates.section).join("\n"),
-        footer: templates.footer(),
-    });
-    await fs.writeFile(outputPath, prettify(output));
+export async function render({
+  sections,
+  meta,
+  distPath = './dist',
+  templatePath,
+  ejsOptions = {
+    openDelimiter: '{',
+    closeDelimiter: '}',
+  },
+}) {
+  const templates = {
+    ...(await parseTemplates(
+      new URL(`./templates`, import.meta.url).pathname,
+      ejsOptions
+    )),
+    ...(await parseTemplates(templatePath, ejsOptions)),
+  };
+
+  const output = templates.main({
+    ...meta,
+    meta: templates.meta({ ...meta }),
+    body: sections.map(templates.section).join('\n'),
+    navbar: templates.navbar({ sections }),
+    footer: templates.footer({ ...meta }),
+  });
+
+  const outputPath = path.join(distPath, 'index.html');
+  await fs.writeFile(outputPath, prettify(output));
 }
